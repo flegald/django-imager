8c282c9b7576f0eaac0c1786316909804da1965f
David Flegal <flegal.david@gmail.com>
2016-04-13 18:53:41 -0700

    working on log in.

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import home_page, login_view
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home_page, name='home_page'),
+    url(r'^login', login_view, name='login'),
 ]
