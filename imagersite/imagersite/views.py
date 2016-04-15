7f82a1456ad7a1f656a9319f2922c71ace8aa902
David Flegal <flegal.david@gmail.com>
2016-04-14 23:28:41 -0700

    fully functional login/out/registration

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,55 +1,31 @@
 # -*- coding: utf-8 -*-
+"""Views handler."""
+
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, render_to_response, redirect
 from django.views.generic import TemplateView
 from registration.forms import RegistrationForm
+from django.contrib.auth import authenticate, login
+from registration.backends.hmac.views import RegistrationView
+
 from django.contrib.auth.forms import AuthenticationForm
 from imager_images.models import Photo
 from django.contrib.auth.models import User
 
 
-
 def home_page(request):
-
+    """Home page view with login."""
     try:
         img = Photo.objects.all().filter(published='public').order_by("?")[0].photo
     except IndexError:
-        img = 'http://purplewalls.com/wp-content/uploads/2016/03/happy-dog-image-93.jpg'
+        img = 'http://orig14.deviantart.net/38de/f/2014/003/c/5/work_harder_comrade__by_pallanoph-d5kk9qt.jpg'
 
     log_in_form = AuthenticationForm()
+    name = request.user.username
     return render(request, 'home.html', context={
         'img': img,
-        'login': log_in_form
+        'login': log_in_form,
+        'name': name
     })
-
-
-def register_page(request):
-    if request.method == "POST":
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            new_user = User.objects.create_user(username=form.data['username'], email=form.data['email'], password=form.data['csrfmiddlewaretoken'],)
-            new_user.save()
-            new_user.backend = 'django.contrib.auth.backends.ModelBackend'
-            login(request, new_user)
-            import pdb; pdb.set_trace()
-            return redirect('/')
-    else:
-        sign_up_form = RegistrationForm()
-        return render(request, 'register.html', context={
-            'sign_up_form': sign_up_form
-        })
-# def log_in(request):
-#     username = request.POST['username']
-#     password = request.POST['password']
-#     user = authenticate(username=username, password=password)
-#     if user is not None:
-#         if user.is_active:
-#             login(request, user)
-#             # Redirect to a success page.
-#         else:
-#             # Return a 'disabled account' error message
-#             ...
-#     else:
-#         # Return an 'invalid login' error message.
