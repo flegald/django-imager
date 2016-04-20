f07597ca5bffb7521aa43dd4e8b019690de6ec77
GingerQ <qiangwenjing@gmail.com>
2016-04-20 10:40:08 -0700

    style applied

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -15,7 +15,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = '/Users/mac/p401/Flegal/django-imager/imagersite/media'
 MEDIA_URL = '/media/'
