b19c239099468c40af6a017685c44d17594ce705
David Flegal <flegal.david@gmail.com>
2016-04-21 15:54:16 -0700

    trying to get test db goin.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     }
 }
 
-if 'test' in sys.argv or 'test_coverage' in sys.argv: 
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
