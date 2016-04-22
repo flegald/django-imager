30c0abeaa733e207aedb4335f3e9fc9c0bc8b68a
GingerQ <qiangwenjing@gmail.com>
2016-04-21 17:00:31 -0700

    css updated requirements minor bugs fixed

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -16,12 +16,12 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.environ['DJANGO_MEDIA']
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # SECURITY WARNING: keep the secret key used in production secret!
