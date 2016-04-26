49b529d0a2171ec7a0ffa61ff53e927d87a9fed9
David Flegal <flegal.david@gmail.com>
2016-04-26 15:45:46 -0700

    static root change.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -16,11 +16,11 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/Users/admin-1/imager/django-imager/imagersite/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
