990377d9cff9acd82838c94753fe1cf8e7e839a5
GingerQ <qiangwenjing@gmail.com>
2016-04-26 14:06:06 -0700

    shhh

--- imagersite/secret.sh
+++ imagersite/secret.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL="postgres://imageruser:passwordqwe@imager-final.cpxt9csnvqot.us-west-2.rds.amazonaws.com:5432"
+export DATABASE_URL="postgres://imageruser:passwordqwe@imager-final.cpxt9csnvqot.us-west-2.rds.amazonaws.com:5432/"
 
 export EMAIL_HOST_USER="wenjing.qiang@yahoo.com"
 
