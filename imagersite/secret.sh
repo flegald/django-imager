e85b4aa4c1714fbb09dbb77e25fa8b38332f1ef9
GingerQ <qiangwenjing@gmail.com>
2016-04-27 11:52:12 -0700

    clean up

--- imagersite/secret.sh
+++
@@ -1,7 +0,0 @@
-export DATABASE_URL="postgres://imageruser:passwordqwe@imager-west-2b.cpxt9csnvqot.us-west-2.rds.amazonaws.com/imagerdatabase"
-
-export EMAIL_HOST_USER="wenjingindjango@gmail.com"
-
-export EMAIL_HOST_PASSWORD="passwordqwe"
-
-export DEFAULT_FROM_EMAIL="nonereply@gmail.com"
