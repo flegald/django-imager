26d7537db85cbea7451e6a3e7e0445ce7e3a632b
David Flegal <flegal.david@gmail.com>
2016-04-29 12:28:55 -0700

    debug.

---
+++ imagersite/imagersite/secrets.sh
@@ -0,0 +1,5 @@
+export DATABASE_URL='postgres://imageruser:1totototo.@imager-us-west-2b.cyatq92yrtow.us-west-2.rds.amazonaws.com:5432'
+
+export EMAIL_HOST_USER='thedino89@gmail.com'
+export EMAIL_HOST_PASSWORD='seattle2015'
+export DEFAULT_FROM_EMAIL='noreply@imagersite.com'
